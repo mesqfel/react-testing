b254089e48474d1ae99376b15f0a2e0aad8767d1
mesqfel <mesquita.superlogica@gmail.com>
2017-05-07 17:03:09 -0300

    A ReactJS component testing sample, using Mocha and Chai

---
+++ test/actions/index_test.js
@@ -0,0 +1,21 @@
+import {expect} from '../test_helper';
+import { SAVE_COMMENT } from '../../src/actions/types';
+import { saveComment } from '../../src/actions/index';
+
+describe('actions', () =>{
+
+    describe('saveComment', () =>{
+
+        it('has the correct type', () =>{
+            const action = saveComment();
+            expect(action.type).to.equal(SAVE_COMMENT);
+        });
+
+        it('has the correct payload', () =>{
+            const action = saveComment('my new comment');
+            expect(action.payload).to.equal('my new comment');
+        });
+
+    });
+
+});
